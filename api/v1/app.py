9fae890f4ba40e0ac8d953ff73f01e2bf421f814
stanley <stanley6453@gmail.com>
2023-04-29 21:32:01 +0100

    modified api/v1/app.py to include cors headers

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -20,6 +21,7 @@ else:
 
 
 app = Flask(__name__)
+CORS(app, resources={"/*": {"origin": "0.0.0.0"}})
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
