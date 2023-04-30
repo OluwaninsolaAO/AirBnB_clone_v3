c75ea9f00392f4a9290932f034ae0a5b66c9aee0
stanley <stanley6453@gmail.com>
2023-04-30 07:38:05 +0100

    modified models/uer.py to hash user password when user is created

--- models/user.py
+++ models/user.py
@@ -28,5 +28,15 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-        self.password = hashlib.md5(
+        self.__password = hashlib.md5(
             kwargs['password'].encode('utf8')).hexdigest()
+
+    @property
+    def password(self):
+        "returns the user's password"
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        self.__password = hashlib.md5(
+            value.encode('utf8')).hexdigest()
