90990af79249bf8085fc8f87936d68100c3e6de7
stanley <stanley6453@gmail.com>
2023-04-30 07:41:05 +0100

    modified models/uer.py to hash user password when user is created

--- models/base_model.py
+++ models/base_model.py
@@ -68,6 +68,8 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if new_dict["__class__"] == "User" and not saveAsFile:
+            del new_dict["__password"]
         return new_dict
 
     def delete(self):
